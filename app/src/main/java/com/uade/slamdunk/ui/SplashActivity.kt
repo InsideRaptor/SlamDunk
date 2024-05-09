092fd62c2e966847c9208ebcba558ff3da4e16a6
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2024-05-08 21:24:45 -0300

    splash first design

--- app/src/main/java/com/uade/slamdunk/ui/SplashActivity.kt
+++ app/src/main/java/com/uade/slamdunk/ui/SplashActivity.kt
@@ -1,10 +1,14 @@
-package com.uade.slamdunk
+package com.uade.slamdunk.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.uade.slamdunk.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,12 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+
     }
 }
