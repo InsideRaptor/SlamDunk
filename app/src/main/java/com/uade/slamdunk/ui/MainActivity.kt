aba2e49f126f3f97f10d4ce9efad1e249cd26d00
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2024-05-10 01:25:15 -0300

    fragments structure refactor

--- app/src/main/java/com/uade/slamdunk/ui/MainActivity.kt
+++ app/src/main/java/com/uade/slamdunk/ui/MainActivity.kt
@@ -5,17 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.uade.slamdunk.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        loadFragment(HomeFragment())
+        bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)!!
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.menu_bookmark -> {
+                    loadFragment(BookmarkFragment())
+                    true
+                }
+                R.id.menu_search -> {
+                    loadFragment(SearchFragment())
+                    true
+                }
+
+                else -> false
+            }
         }
+        bottomNav.selectedItemId = R.id.menu_home
     }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
 }
