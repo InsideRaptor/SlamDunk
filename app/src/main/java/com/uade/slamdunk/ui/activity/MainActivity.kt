7391f0e05a834af5c5406e271af71d42bf1794ce
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2024-05-18 22:54:54 -0300

    main activity list functionality

--- app/src/main/java/com/uade/slamdunk/ui/activity/MainActivity.kt
+++ app/src/main/java/com/uade/slamdunk/ui/activity/MainActivity.kt
@@ -1,19 +1,37 @@
-package com.uade.slamdunk.ui
+package com.uade.slamdunk.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.uade.slamdunk.R
+import com.uade.slamdunk.ui.viewmodel.MainActivityViewModel
+import com.uade.slamdunk.ui.fragment.BookmarkFragment
+import com.uade.slamdunk.ui.fragment.HomeFragment
+import com.uade.slamdunk.ui.fragment.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNav : BottomNavigationView
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loadFragment(HomeFragment())
+
+        bottomNavBar()
+
+        viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        //viewModel.init()
+        viewModel.teams.observe(this) { teams ->
+            // Do something with the fetched teams if needed
+        }
+
+    }
+
+    private fun bottomNavBar() {
         bottomNav = findViewById(R.id.bottomNav)!!
         bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
@@ -29,16 +47,15 @@ class MainActivity : AppCompatActivity() {
                     loadFragment(SearchFragment())
                     true
                 }
-
                 else -> false
             }
         }
         bottomNav.selectedItemId = R.id.menu_home
     }
-    private  fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container,fragment)
-        transaction.commit()
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container,fragment)
+            .commit()
     }
 
 }
