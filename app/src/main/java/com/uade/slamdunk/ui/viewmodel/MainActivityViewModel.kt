99d7c2b788ef84af8ef4e36532f92e43e05f3314
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2024-05-18 22:54:34 -0300

    main viewmodel

---
+++ app/src/main/java/com/uade/slamdunk/ui/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,59 @@
+package com.uade.slamdunk.ui.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.uade.slamdunk.data.NbaRepository
+import com.uade.slamdunk.model.Player
+import com.uade.slamdunk.model.Team
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class MainActivityViewModel : ViewModel() {
+
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.Main + job)
+
+    val TAG = "NBA_API"
+
+    private val nbaRepo: NbaRepository = NbaRepository()
+    var teams: MutableLiveData<ArrayList<Team>> = MutableLiveData<ArrayList<Team>>()
+    var filteredTeams: MutableLiveData<ArrayList<Team>> = MutableLiveData<ArrayList<Team>>()
+    var players: MutableLiveData<ArrayList<Player>> = MutableLiveData<ArrayList<Player>>()
+
+    init {
+        fetchTeams()
+    }
+
+    // Recuperar a los equipos desde la API
+    private fun fetchTeams() {
+        scope.launch {
+            kotlin.runCatching {
+                nbaRepo.getTeams()
+            }.onSuccess {
+                Log.d(TAG, "onSuccess")
+                teams.postValue(it)
+                filteredTeams.postValue(it)
+                Log.d(TAG, it.toString())
+            }.onFailure {
+                Log.d(TAG, "Error: $it")
+                teams.postValue(ArrayList())
+                filteredTeams.postValue(ArrayList())
+            }
+        }
+    }
+    fun filterTeams(query: String) {
+        val filteredList = teams.value?.filter {
+            it.name.contains(query, ignoreCase = true)
+        } ?: ArrayList()
+        filteredTeams.postValue(filteredList as ArrayList<Team>?)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
+}
