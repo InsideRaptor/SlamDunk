7b7cc9261c62dff6278f8ab728fe1006dcdfdbcf
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2024-06-08 20:34:02 -0300

    logout functionality

--- app/src/main/java/com/uade/slamdunk/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/uade/slamdunk/ui/fragment/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.uade.slamdunk.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.uade.slamdunk.R
+import com.uade.slamdunk.ui.activity.LoginActivity
 import com.uade.slamdunk.ui.adapter.NbaAdapter
 import com.uade.slamdunk.ui.viewmodel.MainActivityViewModel
 class HomeFragment : Fragment() {
@@ -18,6 +22,7 @@ class HomeFragment : Fragment() {
     private lateinit var rvMain: RecyclerView
     private lateinit var adapter: NbaAdapter
     private lateinit var searchBar: SearchView
+    private lateinit var btnLogout: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,11 +41,27 @@ class HomeFragment : Fragment() {
         searchBar = view.findViewById(R.id.searchView)
         setupSearchBar()
 
+        // Logout functionality
+        btnLogout = view.findViewById(R.id.btnLogout)
+        btnLogout.setOnClickListener {
+            logout()
+        }
+
         viewModel.filteredTeams.observe(viewLifecycleOwner) { filteredTeams ->
             adapter.updateItems(filteredTeams)
         }
 
         return view
+    }
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut()
+
+        // Navigate back to LoginActivity
+        val intent = Intent(activity, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        activity?.finish()
     }
 
     private fun setupSearchBar() {
