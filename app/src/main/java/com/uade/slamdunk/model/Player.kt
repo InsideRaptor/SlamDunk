fd18669b597564d2f86f103eb4e5139e054b19c9
insideraptor <manchitaspro@gmail.com>
2024-06-30 01:34:02 -0300

    remove unnecesary parameters

--- app/src/main/java/com/uade/slamdunk/model/Player.kt
+++ app/src/main/java/com/uade/slamdunk/model/Player.kt
@@ -4,35 +4,35 @@ data class Player(
     val id: Int,
     val firstname: String,
     val lastname: String,
-    val birth: Birth,
-    val nba: Nba,
-    val height: Height,
-    val weight: Weight,
-    val college: String,
-    val affiliation: String,
-    val leagues: Leagues
+//    val birth: Birth,
+//    val nba: Nba,
+//    val height: Height,
+//    val weight: Weight,
+//    val college: String,
+//    val affiliation: String,
+//    val leagues: Leagues
 )
 
-data class Birth(
-    val date: String,
-    val country: String
-)
-
-data class Nba(
-    val start: Int,
-    val pro: Int
-)
-
-data class Height(
-    val feets: String,
-    val inches: String,
-    val meters: String
-)
-
-data class Weight(
-    val pounds: String,
-    val kilograms: String
-)
+//data class Birth(
+//    val date: String,
+//    val country: String
+//)
+//
+//data class Nba(
+//    val start: Int,
+//    val pro: Int
+//)
+//
+//data class Height(
+//    val feets: String,
+//    val inches: String,
+//    val meters: String
+//)
+//
+//data class Weight(
+//    val pounds: String,
+//    val kilograms: String
+//)
 
 data class JSONResponsePlayer (
     val response: List<Player>
